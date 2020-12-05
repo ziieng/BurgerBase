f394067affa3bd32508c2794e4d600ef0fc6d159
ziieng <cjengelhardt@gmail.com>
2020-12-05 13:53:39 -0800

    correct server initialization

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var express = require("express");
 var exphbs = require("express-handlebars");
-var mysql = require("mysql");
 
 var app = express();
 
@@ -15,18 +14,13 @@ app.use(express.static("public"));
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "quotes_db",
-});
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller");
 
-connection.connect(function (err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+app.use(routes);
+
+// Start server
+app.listen(PORT, function () {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
 });
