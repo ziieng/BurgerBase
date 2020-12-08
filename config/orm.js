5f43d1d8d64e29883b4ab40d28e59dab566ed666
ziieng <cjengelhardt@gmail.com>
2020-12-07 20:16:17 -0800

    setup initial ORM queries

--- config/orm.js
+++ config/orm.js
@@ -1,11 +1,34 @@
 const connection = require("./connection");
 
 const orm = {
-  selectAll() {},
+  selectAll(table, filtercol, filtercond, cb) {
+    connection.query(
+      `SELECT * FROM ${table} WHERE ${filtercol} = ${filtercond}`,
+      (err,   res) => {
+            if (err) console.log(err);
+            cb(res);
+          }
+    );
+  },
 
-  insertOne() {},
+  insertOne(intable, conds, values, cb) {
+    connection.query(
+      `INSERT INTO ${intable} (${conds}) VALUES ?`,
+      values,
+      (err,   res) => {
+            if (err) console.log(err);
+            cb(res);
+          }
+    );
+  },
 
-  updateOne() {},
+  updateOne(table,upcol,upval,upid, cb) {
+    connection.query(`UPDATE ${table} SET ${upcol} = ${upval} WHERE id = ${upid}`,
+    (err, res) => {
+      if (err) console.log(err);
+      cb(res);
+    });
+  },
 };
 
 module.exports = orm;
