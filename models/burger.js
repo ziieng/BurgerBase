87c2188434748293c4615f4174702f8d0e7f1eb1
ziieng <cjengelhardt@gmail.com>
2020-12-07 20:30:17 -0800

    setup model functions

--- models/burger.js
+++ models/burger.js
@@ -1,8 +1,21 @@
 const orm = require("../config/orm");
 
-//calls to orm functions
-// orm.insertOne()
-// orm.selectAll()
-// orm.updateOne()
+var burger = {
+  //calls to orm functions
+  //list all devoured or undevoured
+  all: function (status, cb) {
+    orm.selectAll("burgers", "devoured", status, (res) => cb(res));
+  },
 
-// module.exports = calls
+  //insert new
+  insert: function (data, cb) {
+    orm.insertOne("burgers", "burger_name, icon", data, (res) => cb(res));
+  },
+
+  //update devoured status
+  devour: function  (id, cb) {
+    orm.updateOne("burgers",  "devoured",  "true",  id, (res)  =>  cb(res));;
+  },,
+};
+
+module.exports = burger;
